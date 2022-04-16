8eec7aa8dc51c5c851faa666af7b39efb1041c48
DerekRin <derekrin98@gmail.com>
2022-04-16 11:36:49 -0500

    ppwfdv

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
+//Check Pro-Read-Gen for file generation
+
+var companyArray = [];
+
 questions();
 
 function questions() {
@@ -46,69 +50,29 @@ function questions() {
               answers.Memail,
               answers.officenumber
             );
+            companyArray.push(manager);
+            company();
             console.log(manager);
-            inquirer
-              .prompt([
-                {
-                  name: "roleselect",
-                  message: "What else do you want to add?",
-                  type: "list",
-                  choices: ["Engineer", "Intern", "Finish"],
-                },
-              ])
-              .then((answers) => {
-                if (answers.roleselect === "Engineer") {
-                  inquirer
-                    .prompt([
-                      {
-                        name: "nameE",
-                        message: "What is your name?",
-                        type: "input",
-                      },
-                      {
-                        name: "idE",
-                        message: "What is your id?",
-                        type: "input",
-                      },
-                      {
-                        name: "emailE",
-                        message: "What is your email?",
-                        type: "input",
-                      },
-                      {
-                        name: "github",
-                        message: "What is your github?",
-                        type: "input",
-                      },
-                    ])
-                    .then((answers) => {
-                      const engineer = new Engineer(
-                        answers.nameE,
-                        answers.idE,
-                        answers.emailE,
-                        answers.github
-                      );
-                      console.log(engineer);
-                    });
-                }
-                console.log(answers);
-              });
-          });
-      } else {
-        inquirer
-          .prompt([
-            {
-              name: "typeE",
-              message: "Would you like to add an engineer?",
-              type: "confirm",
-            },
-          ])
-          .then((answers) => {
-            if (answers.typeE === true) {
-              getEngineer();
-            }
           });
       }
+    });
+}
+function company() {
+  inquirer
+    .prompt([
+      {
+        name: "roleselect",
+        message: "What else do you want to add?",
+        type: "list",
+        choices: ["Engineer", "Intern", "Finish"],
+      },
+    ])
+    .then((answers) => {
+      if (answers.roleselect === "Engineer") {
+        getEngineer();
+      } else if (answers.roleselect === "Intern") {
+        getIntern();
+      } else if (answers.roleselect === "Finish") console.log(companyArray);
     });
 }
 
@@ -131,11 +95,42 @@ function questions() {
 //     });
 // });
 
-function getEngineer() {}
-
-// function getManager() {
-
-// }
+function getEngineer() {
+  inquirer
+    .prompt([
+      {
+        name: "nameE",
+        message: "What is your name?",
+        type: "input",
+      },
+      {
+        name: "idE",
+        message: "What is your id?",
+        type: "input",
+      },
+      {
+        name: "emailE",
+        message: "What is your email?",
+        type: "input",
+      },
+      {
+        name: "github",
+        message: "What is your github?",
+        type: "input",
+      },
+    ])
+    .then((answers) => {
+      const engineer = new Engineer(
+        answers.nameE,
+        answers.idE,
+        answers.emailE,
+        answers.github
+      );
+      companyArray.push(engineer);
+      company();
+      console.log(engineer);
+    });
+}
 
 function getIntern() {
   inquirer
@@ -168,6 +163,8 @@ function getIntern() {
         answers.Iemail,
         answers.school
       );
+      companyArray.push(intern);
+      company();
       console.log(intern);
     });
 }
